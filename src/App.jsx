fc7834562781c42cad578071a21c4d54997ea858
Anushka De <anushkad@stanford.edu>
2024-07-15 19:06:34 -0700

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import Hero from './components/Hero';
-
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle('dark', darkMode);
+  }, [darkMode]);
+  
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  
   return (
-    <div className="fun min-h-screen">
-      <div className="max-w-7xl mx-auto pt-20 px-6">
-        <Hero />
+    <div className={`${darkMode ? 'dark' : ''}`}>
+      <div className="fun min-h-screen">
+        <div className="max-w-7xl mx-auto pt-20 px-6">
+          {/* <div id="toggle" onClick={toggleDarkMode}>
+            <div className="indicator"></div>
+          </div> */}
+          <Hero />
+        </div>
       </div>
+
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Hero = () => {
-  return (
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle('dark', darkMode);
+  }, [darkMode]);
+  
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  
+
+  return ( 
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="hover:card-wrapper h-screen w-11/12 rounded-5xl shadow-lg">
-        <div className="card-content flex flex-col">
+      <div className="hover:card-wrapper h-screen w-11/12 rounded-5xl shadow-2xl dark:shadow-gray-600 dark:bg-gray-800">
+        <div className="card-content flex flex-col dark:text-white transition-colors duration-200">
+          <div id="toggle" onClick={toggleDarkMode}>
+            <div className="indicator"></div>
+          </div>
           <div className="flex flex-row w-full h-full">
 
             <div className="flex flex-col p-6">
@@ -12,14 +26,14 @@ const Hero = () => {
             </div>
 
             <div className="flex flex-col mt-40 md:mt-72 md:fixed left-1/2">
-              <h1 className="text-2xl md:text-3xl font-bold text-shadow-lg">Anushka De</h1>
-              <p className="md:mt-10 text-sm md:text-md font-light md:fixed">
+              <h1 className="text-2xl md:text-3xl font-bold text-shadow-lg dark:text-white transition-colors duration-50">Anushka De</h1>
+              <p className="md:mt-10 text-sm md:text-md font-light md:fixed dark:text-gray-300">
                 Computer Science @ 
-                <span className="bg-gradient-to-r font-bold from-red-800 to-red-700 text-transparent bg-clip-text">
+                <span className="bg-gradient-to-r font-bold from-red-800 to-red-700 dark:from-red-600 dark:to-red-600 text-transparent bg-clip-text transition-colors duration-200">
                   {" "}Stanford
                 </span>
               </p>
-              <div className="mt-2 md:mt-8 text-xs lg:text-sm text-slate-400 flex justify-center" style={{ marginLeft: '-0.75rem' }}>
+              <div className="mt-2 md:mt-8 text-xs lg:text-sm text-gray-400 dark:text-gray-300 flex justify-center" style={{ marginLeft: '-0.75rem' }}>
                 <a href="https://www.linkedin.com/in/anushkadde/" className="relative transition duration-300 ease-in-out hover:bg-gradient-to-r from-red-800 to-red-700 hover:text-white px-3 py-1 rounded-full" target="_blank">
                   <span className="hover:text-white">LinkedIn</span>
                 </a>
@@ -38,6 +52,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
+
 
       {/* <div className="card-wrapper h-screen w-11/12 rounded-5xl shadow-lg">
         
